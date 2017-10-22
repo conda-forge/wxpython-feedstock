a74a6af3c24bed0b44e34b7fdef6ff7a52ad8c11
Korijn van Golen <korijn@gmail.com>
2017-10-22 02:28:32 +0200

    Update run_docker_build.sh

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -63,7 +63,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line be updated
 # automatically.
-/usr/bin/sudo -n yum search gtk3
+/usr/bin/sudo -n yum search gtk
 /usr/bin/sudo -n yum install -y gtk3-devel webkitgtk-devel
 
 
