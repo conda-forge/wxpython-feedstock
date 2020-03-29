dda7e8ffbfbfabf4a21e57085d4d4d73783257ed
Tim Snyder <snyder.tim@gmail.com>
2020-03-29 09:55:34 -0700

    adjust windows installation to match osx and linux
    * install everything into single wxpython package. linux and osx
      include the wxWidgets headers and libs in the single package
    * don't hardcode the lib/vc140_dll directory name, assume there
      will be one subdirectory matching lib/vc* and that it will
      change based on parameters passed to the wxWidgets build
      script.  Currently it is lib/vc140_x86_dll.  Will change to
      vc141 once we switch to vc2017.
    * check for errorlevel after most commands in the install script.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
 setlocal EnableDelayedExpansion
 
-"%PYTHON%" build.py build_wx install_wx --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
+"%PYTHON%" build.py build_wx --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
 if errorlevel 1 exit 1
+call "%RECIPE_DIR%"\install_wxwidgets.bat
+if errorlevel 1 exit 1
+
 "%PYTHON%" build.py build_py install_py --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
 if errorlevel 1 exit 1
