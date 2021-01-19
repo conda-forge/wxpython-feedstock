047c386a4cf9e52af664c1cd38be0759a9e250e1
Uwe L. Korn <uwe.korn@quantco.com>
2021-01-19 16:47:02 +0100

    Less hacky patch on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,10 @@
+@echo on
 setlocal EnableDelayedExpansion
 
 "%PYTHON%" build.py build_wx --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
 if errorlevel 1 exit 1
 call "%RECIPE_DIR%"\install_wxwidgets.bat
 if errorlevel 1 exit 1
-
-"%PYTHON%" -c "f = open('./ext/wxWidgets/include/wx/msw/wx.rc', 'r'); txt = f.readlines(); f.close(); txt[124] = '#define wxMANIFEST_CPU amd64'; txt[123] = '#define WX_CPU_AMD64'; f = open('./ext/wxWidgets/include/wx/msw/wx.rc', 'w'); f.writelines(txt); f.close()"
 
 "%PYTHON%" build.py build_py install_py --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%" --extra_make="-v" --verbose
 if errorlevel 1 exit 1
