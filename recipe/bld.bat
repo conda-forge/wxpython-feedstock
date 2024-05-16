86abc2975800c42b6e2f2be0d0afef594b8966ce
Paul McCarthy <pauldmccarthy@gmail.com>
2024-05-16 16:28:26 +0100

    RF: Change recipe to build against conda-forge wxwidgets, rather than compiling an internal version of wxwidgets.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,12 +4,11 @@ setlocal EnableDelayedExpansion
 :: The siplib files contained in the 4.2.1 tarball are incompatible
 :: with python 3.12, so need to be re-generated
 :: https://github.com/wxWidgets/Phoenix/issues/2455
-"%PYTHON%" build.py sip      --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
+"%PYTHON%" build.py sip      --no_magic --use_syswx --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
 
-"%PYTHON%" build.py build_wx --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
 if errorlevel 1 exit 1
 call "%RECIPE_DIR%"\install_wxwidgets.bat
 if errorlevel 1 exit 1
 
-"%PYTHON%" build.py build_py install_py --no_magic --prefix="%PREFIX%" --jobs="%CPU_COUNT%" --extra_make="-v" --verbose
+"%PYTHON%" build.py build_py install_py --no_magic --use_syswx --prefix="%PREFIX%" --jobs="%CPU_COUNT%" --extra_make="-v" --verbose
 if errorlevel 1 exit 1
