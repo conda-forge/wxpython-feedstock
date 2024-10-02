7f29a900f2f90617b805d44a4a31f508509608cb
Paul McCarthy <pauldmccarthy@gmail.com>
2024-10-02 10:04:57 +0100

    Remove unnecessary sip regen in windows build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,11 +11,5 @@ setlocal EnableDelayedExpansion
 xcopy /y /e /s  %PREFIX%\Library\lib\vc_x64_dll ext\wxWidgets\lib\vc140_x64_dll\
 if errorlevel 1 exit 1
 
-:: The siplib files contained in the 4.2.1 tarball are incompatible
-:: with python 3.12, so need to be re-generated
-:: https://github.com/wxWidgets/Phoenix/issues/2455
-"%PYTHON%" build.py sip      --no_magic --use_syswx --prefix="%PREFIX%" --jobs="%CPU_COUNT%"
-if errorlevel 1 exit 1
-
 "%PYTHON%" build.py build_py install_py --no_magic --use_syswx --prefix="%PREFIX%" --jobs="%CPU_COUNT%" --extra_make="-v" --verbose
 if errorlevel 1 exit 1
